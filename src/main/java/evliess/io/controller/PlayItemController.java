545cbfbd99fef8831d2d80127d85bf67d45d9354
xiaofei <250079652@qq.com>
2025-08-20 14:11:55 +0800

    250820: support play-items

--- src/main/java/evliess/io/controller/PlayItemController.java
+++ src/main/java/evliess/io/controller/PlayItemController.java
@@ -4,6 +4,10 @@ package evliess.io.controller;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import evliess.io.service.PlayItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +29,13 @@ public class PlayItemController {
         return ResponseEntity.ok("hello");
     }
 
+    @Operation(summary = "新增一个项目")
+    @Parameters({
+        @Parameter(name = "body",
+                description = "{<br>" +
+                        "name: 寻宝,<br>" +
+                        "money: 39.9<br>}")
+    })
     @PostMapping("/play-items")
     public ResponseEntity<String> create(@RequestBody String body) {
         JSONObject jsonNode = JSON.parseObject(body);
@@ -32,7 +43,10 @@ public class PlayItemController {
         String money = jsonNode.getString("money");
         return playItemService.save(name, money);
     }
-
+    @Operation(summary = "根据项目名字修改价格")
+    @Parameters({
+            @Parameter(name = "name", description = "项目名字", example = "寻宝"),
+            @Parameter(name = "money", description = "收费", example = "39.9")})
     @PutMapping("/play-items/{name}/{money}")
     public ResponseEntity<String> update(@PathVariable("name") String name,
                                          @PathVariable("money") String money) {
@@ -41,6 +55,10 @@ public class PlayItemController {
         return playItemService.updateByName(name, money);
     }
 
+    @Operation(summary = "根据项目名字查看项目")
+    @Parameters({
+            @Parameter(name = "name", description = "项目名字", example = "寻宝"),
+            })
     @GetMapping("/play-items/{name}")
     public ResponseEntity<String> findByName(@PathVariable("name") String name) {
         if (name == null || name.isEmpty())
@@ -48,6 +66,10 @@ public class PlayItemController {
         return playItemService.findByName(name);
     }
 
+    @Operation(summary = "根据项目名字删除项目")
+    @Parameters({
+            @Parameter(name = "name", description = "项目名字", example = "寻宝"),
+    })
     @DeleteMapping("/play-items/{name}")
     public ResponseEntity<String> deleteByName(@PathVariable("name") String name) {
         if (name == null || name.isEmpty())
