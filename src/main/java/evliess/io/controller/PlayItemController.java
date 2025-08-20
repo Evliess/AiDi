274e19959fcb972d45e28f264ddf2d6589152c90
xiaofei <250079652@qq.com>
2025-08-20 14:12:08 +0800

    250820: support play-items

--- src/main/java/evliess/io/controller/PlayItemController.java
+++ src/main/java/evliess/io/controller/PlayItemController.java
@@ -7,7 +7,6 @@ import evliess.io.service.PlayItemService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
